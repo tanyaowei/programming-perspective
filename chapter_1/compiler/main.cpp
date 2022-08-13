f47bbb22be787be7a2d745036dcb4dc18db10fb8
Tan Yao Wei <blazex.net@hotmail.com>
2022-08-13 09:16:19 +0800

    added template and macro example into chapter 1

--- chapter_1/compiler/main.cpp
+++ chapter_1/compiler/main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 
+template<typename T1, typename T2>
+T2 sum(T1 a, T2 b)
+{
+    return static_cast<T2>(a) + b;
+}
+
 int main(int argc, char** argv)
 {
-    std::cout << "hello world!" << std::endl;
+    std::cout << sum(1,1.0) << " hello world! " << sum(5.0f,5) << std::endl;
 
     return 0;
 }
--- chapter_1/preprocessor/main.cpp
+++ chapter_1/preprocessor/main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 
+#define circleArea(r) (3.1415*(r)*(r)) 
+
+template<typename T>
+T sum(T a, T b)
+{
+    return a + b;
+}
+
 int main(int argc, char** argv)
 {
-    std::cout << "hello world!" << std::endl;
+    std::cout << "hello world! " << circleArea(sum(5,5)) << std::endl;
 
     return 0;
 }
